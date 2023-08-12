4499dbbf6d2f7bf0ab2211a536b51d1fd6ef3f35
sonyansh <sonyaistsocial@gmail.com>
2023-08-12 17:03:24 +0300

    exam

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+const { test, toBeTruthy() } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
     await page.goto('http://localhost:8080/collection');
